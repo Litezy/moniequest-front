c1f40e21adfacc01f28debd4a9153695f2780bb2
Litezy <liteb237@gmail.com>
2025-03-10 16:41:45 +0100

    changes to env and updates

--- src/GeneralComponents/GoogleSignInButton.jsx
+++ src/GeneralComponents/GoogleSignInButton.jsx
@@ -1,39 +1,35 @@
 import React from "react";
-import { auth, provider, signInWithPopup } from "../utils/firebase";
-import googleLogo from '../assets/images/googleIcon.png';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from '../utils/firebase';
+import googleimg from '../assets/images/googleIcon.png';
 
-const GoogleSignInButton = ({ onSuccess, onFailure,text }) => {
+const GoogleSignButton = ({ onSuccess, onError,text }) => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      const userDetails ={
-        email: user.email,
-        first_name: user.displayName.split(' ')[0],
-        last_name: user.displayName.split(' ')[1],
-        image: user.photoURL,
-      }
-      onSuccess({
-        email: userDetails.email,
-        first_name: userDetails.first_name,
-        lastname: userDetails.last_name,
-        image: userDetails.image,
-      });
+      onSuccess && onSuccess(result.user);
     } catch (error) {
-      console.error("Google Sign-in Error:", error);
-      onFailure(error);
+      console.error("Google Sign-In Error:", error);
+      onError && onError(error);
     }
   };
 
   return (
     <button type="button"
       onClick={handleGoogleSignIn}
-      className="flex lowercase  items-center justify-center w-full p-3 bg-primary text-white rounded-lg shadow-md hover:bg-ash"
+      className="flex items-center w-full justify-center px-4 text-white py-3 bg-primary  rounded-md shadow-sm hover:bg-ash"
     >
-      <img src={googleLogo} alt="Google Icon" className="w-5 h-5 mr-2" />
-      {text}
+      <img
+        src={googleimg}
+        alt="Google Logo"
+        className="w-5 h-5 mr-2"
+      />
+        {text}
     </button>
   );
 };
 
-export default GoogleSignInButton;
+export default GoogleSignButton;
